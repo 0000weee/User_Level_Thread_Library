b2e53677c2165ba084bf5af5e613943893b30967
Chi Hung Hsieh <adamare0000@gmail.com>
2024-12-07 17:39:13 +0800

    增加scheduler.c 的catch signal name邏輯

--- src/scheduler.c
+++ src/scheduler.c
@@ -11,11 +11,24 @@
 // Prints out the signal you received.
 // This function should not return. Instead, jumps to the scheduler.
 void sighandler(int signum) {
-    // Your code here
+    if (signum == SIGTSTP){
+        // 處理 SIGTSTP 信號
+        printf("caught SIGTSTP\n");
+
+    }else if(signum == SIGALRM){
+        // 處理 SIGALRM 信號
+        printf("caught SIGALRM\n");
+    }else{
+        // 處理 default signal
+    }
 }
 
 // TODO::
 // Perfectly setting up your scheduler.
 void scheduler() {
     // Your code here
+    if (setjmp(sched_buf) == 0){ // Call setjmp() to save the scheduler's context
+        //Create the idle thread with ID 0 and the routine idle().
+        idle(0, NULL);
+    }
 }
